b26966dc7b92e2fcbc98842aa7bbfb357c496205
Joseph Li <joeli1989@gmail.com>
2014-06-12 13:52:32 +0800

    3rd commit with images pulled from youtube.

--- inc/videos.php
+++ inc/videos.php
@@ -5,31 +5,31 @@
 			"name" => "Amazing Highlights NCAA College Football 2012-2013",
 			"type" => "sport",
 			"video" => "http://www.youtube.com/watch?v=i55Zs30ops8",
-			"img" => "assets/images/thumbnails/sport_01.jpg"
+			"img" => "//i1.ytimg.com/vi_webp/i55Zs30ops8/mqdefault.webp"
 		);
 		$videos[2] = array(
 			"name" => "Awe - Jason Silva",
 			"type" => "life",
 			"video" => "https://www.youtube.com/watch?v=8QyVZrV3d3o",
-			"img" => "assets/images/thumbnails/video2.png"
+			"img" => "//i1.ytimg.com/vi/8QyVZrV3d3o/mqdefault.jpg"
 		);
 		$videos[3] = array(
 			"name" => "Steve Jobs - Standford University Speech 2005",
 			"type" => "life",
 			"video" => "https://www.youtube.com/watch?v=UF8uR6Z6KLc",
-			"img" => "assets/images/thumbnails/video3.png"
+			"img" => "//i1.ytimg.com/vi/UF8uR6Z6KLc/mqdefault.jpg"
 		);
 		$videos[4] = array(
 			"name" => "Will Smith - Life",
 			"type" => "life",
 			"video" => "https://www.youtube.com/watch?v=fAKsgBP36V0",
-			"img" => "assets/images/thumbnails/video4.png"
+			"img" => "//i1.ytimg.com/vi/fAKsgBP36V0/mqdefault.jpg"
 		);
 		$videos[5] = array(
 			"name" => "The Game before The Game ",
 			"type" => "sport",
 			"video" => "https://www.youtube.com/watch?v=v_i3Lcjli84",
-			"img" => "assets/images/thumbnails/video5.png"
+			"img" => "//i1.ytimg.com/vi_webp/v_i3Lcjli84/mqdefault.webp"
 		);
 		
 
